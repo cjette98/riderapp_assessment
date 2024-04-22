c9fae5d3fbdef33ae2b743f0a0a2b0b4f32945e3
jcastillo <developerclaymind@gmail.com>
2024-04-22 10:03:25 +0800

    feat: integrate react-native-maps

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet} from 'react-native';
+import MapView, {Region} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function Home() {
-  return <></>;
+  const [region, setRegion] = useState<Region>();
+
+  function getInitialState() {
+    Geolocation.getCurrentPosition(info => {
+      const {latitude, longitude} = info.coords;
+      setRegion({
+        latitude,
+        longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    });
+  }
+
+  function onRegionChange(value: Region) {
+    // setRegion(value);
+  }
+
+  useEffect(() => {
+    getInitialState();
+  }, []);
+
+  return (
+    <MapView
+      region={region}
+      onRegionChange={onRegionChange}
+      style={styles.map}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
